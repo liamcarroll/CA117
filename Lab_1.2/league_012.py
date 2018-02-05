d06d62fab043ea59f48bd8e6bfd5b58943c997b6
Liam Carroll <liamcarroll36@mail.dcu.ie>
2018-02-05 17:55:22 +0000

    Lab 1.2

--- Lab_1.2/league_012.py
+++ Lab_1.2/league_012.py
@@ -3,22 +3,26 @@
 import sys
 
 def longest_club(table):
-   len_longest_club = 0
+   longest = 0
    for line in table:
       tokens = line.split()
-      i = 1
-      while tokens[i].isalpha():
-         i += 1
-   club_name = ' '.join(tokens[1:i])
-   if len_longest_club < len(club_name):
-      len_longest_club = len(club_name)
+      club_name = ' '.join(tokens[1:-8])
+      if longest < len(club_name):
+         longest = len(club_name)
+   return longest
 
 def main():
-   lines = []
+   table = []
    for line in sys.stdin:
-      lines.append(line.strip())
-   club_width = longest_club(lines)
-   print('{} {:<{}} {:>2} {:>3} {:>3} {:>3} {:>3} {:>3} {:>3} {:>3} '.format('POS','CLUB',club_width,'P','W','D','L','GF','GA','GD','PTS'))
+      table.append(line.strip())
+   club_width = longest_club(table)
+   print('{} {:<{}} {:>2} {:>3} {:>3} {:>3} {:>3} {:>3} {:>3} {:>3}'.format('POS','CLUB',club_width,'P','W','D','L','GF','GA','GD','PTS'))
+   
+   for line in table:
+      tokens = line.split()
+      print(tokens)
+      club_name = tokens[1:-8]
+      print('{:>3d} {:<{}} {:>2d} {:>3d} {:>3d} {:>3d} {:>3d} {:>3d} {:>3d} {:>3d}'.format(tokens[0], club_name, club_width, tokens[-8], tokens[-7], tokens[-6], tokens[-5], tokens[-4], tokens[-3], tokens[-2], tokens[-1]))
 
 if __name__ == '__main__':
    main()
