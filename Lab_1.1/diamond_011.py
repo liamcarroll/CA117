58b6a0bbfbed8cb0cde92337898237b248e79c38
Liam Carroll <liamcarroll36@mail.dcu.ie>
2018-02-05 10:13:11 +0000

    Lab 1.1

--- Lab_1.1/diamond_011.py
+++ Lab_1.1/diamond_011.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
+import sys
+
 def diamond(size):
-   
+   for i in range(-size+1,size):
+      i = abs(i)
+      print(end=' ' * i)
+      print(' '.join(['*'] * (size-i)))
 
 def main():
    n = int(sys.argv[1])
    diamond(n)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
    main()
--- chop_011.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-for line in sys.stdin:
-   new_string = line.strip()[1:-1]
-   if 0 < len(new_string):
-      print(new_string)
