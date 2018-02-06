e800836aa86c6a697b794d8bc346ad296d4b33bf
Liam Carroll <liam.carroll36@mail.dcu.ie>
2018-02-06 23:09:33 +0000

    Lab 2.1

--- Lab_2.1/poker_021.py
+++ Lab_2.1/poker_021.py
@@ -2,6 +2,43 @@
 
 import sys
 
+totals = {'total_0': 0,
+          'total_1': 0,
+          'total_2': 0,
+          'total_3': 0,
+          'total_4': 0,
+          'total_5': 0,
+          'total_6': 0,
+          'total_7': 0,
+          'total_8': 0,
+          'total_9': 0}
 
-for i in range(10):
-    
+
+ranks = {0: 'nothing',
+         1: 'one pair',
+         2: 'two pairs',
+         3: 'three of a kind',
+         4: 'a straight',
+         5: 'a flush',
+         6: 'a full house',
+         7: 'four of a kind',
+         8: 'a straight flush',
+         9: 'a royal flush'}
+
+
+def main():
+    for line in sys.stdin:
+        rank = line.strip()[-1]
+        totals['total_{}'.format(rank)] += 1
+
+    total_hands = 0
+    for key in totals:
+        total_hands += totals[key]
+
+    for i in range(10):
+        frequency = totals['total_{}'.format(i)]
+        probability = (frequency / total_hands) * 100
+        print('The probability of {} is {:.4f}%'.format(ranks[i], probability))
+
+if __name__ == '__main__':
+    main()
