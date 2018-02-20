eda88ed8e201129df8dd1e4b77f880c0a8557453
Liam Carroll <liam.carroll36@mail.dcu.ie>
2018-02-20 18:31:16 +0000

    Lab 4.1

--- Lab_4.1/main_words_041.py
+++ Lab_4.1/main_words_041.py
@@ -3,24 +3,30 @@
 import sys
 import string
 
+words = []
+count = {}
+
+
 def main():
-    words = []
-    count = {}
-    longest_w = 0
     for line in sys.stdin:
         line = line.strip().casefold().split()
         for w in line:
             word = w.strip(string.punctuation)
+
             if 3 < len(word) and word not in count:
                 count[word] = 1
                 words.append(word)
             elif 3 < len(word):
                 count[word] += 1
-    words = sorted(words)
+
+    words_3_freq_plus = sorted([key for key in count if 3 <= count[key]])
+
+    longest_w = len(max(words_3_freq_plus, key=len))
     longest_key = len(str(max(count.values())))
-    for w in words:
-        if 3 <= count[w]:
-            print('{:>{:d}s} : {:>{:d}d}'.format(w, longest_w, count[w], longest_key))
+
+    for w in words_3_freq_plus:
+        print('{:>{:d}s} : {:>{:d}d}'.format(
+            w, longest_w, count[w], longest_key))
 
 if __name__ == '__main__':
     main()
---
+++ Lab_4.1/vowels_041.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+vowels = 'aeiou'
+vowel_count = {'a': 0,
+               'e': 0,
+               'i': 0,
+               'o': 0,
+               'u': 0}
+
+
+def vowel_frequencies(s):
+    
+
+
+def main():
+    for line in sys.stdin:
+        line = line.strip()
+        vowel_frequencies(line)
