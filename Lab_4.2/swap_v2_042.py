3c4036c9ebfb6c7b76ebfcc91869781293fcf0c0
Liam Carroll <liam.carroll36@mail.dcu.ie>
2018-02-26 00:08:32 +0000

    Lab 4.2

--- Lab_4.2/swap_v2_042.py
+++ Lab_4.2/swap_v2_042.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+
 def swap_unique_keys_values(d):
     new_d = {}
-    
+    for k, v in d.items():
+        if v not in new_d:
+            new_d[v] = k
+        else:
+            del new_d[v]
+    return new_d
---
+++ Lab_4.2/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import swap_v2_042 as swap
+
+my_dict = {'a' : 4, 'b' : 7, 'c' : 10, 'd' : 7}
+new_dict = swap.swap_unique_keys_values(my_dict)
