209059a5fc52ca93a0a8256233af553cde3b1b8f
Liam Carroll <lcarroll@localhost.localdomain>
2018-03-06 23:58:21 +0000

    Lab 5.2

--- Lab_5.2/q4_052.py
+++ Lab_5.2/q4_052.py
@@ -2,11 +2,18 @@
 
 import sys
 
+
+def sorter(t):
+    return t[1]
+
+
 def main():
     food_cal = {}
     with open(sys.argv[1], 'r') as f:
         for line in f:
-            foodstuff, calories = line.strip().split()
+            tokens = line.strip().split()
+            foodstuff = ' '.join(tokens[:-1])
+            calories = tokens[-1]
             food_cal[foodstuff] = int(calories)
 
     total_cals_per_person = {}
@@ -18,9 +25,13 @@ def main():
         total = 0
         for food in consumed_food:
             try:
-                total += food_cal(food)
+                total += food_cal[food]
             except KeyError:
                 total += 100
         total_cals_per_person[name] = total
 
-    
+    for t in sorted(total_cals_per_person.items(), key=sorter):
+        print('{:>{}s} : {:>{}d}'.format(t[0], len(max(total_cals_per_person.keys(), key=len)), t[1], len(str(max(total_cals_per_person.values())))))
+
+if __name__ == '__main__':
+    main()
