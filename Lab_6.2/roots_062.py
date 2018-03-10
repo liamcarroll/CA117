49c0f4a5b0b1fa18bcf443bf783c4c02bd8e276f
Liam Carroll <liam.carroll36@mail.dcu.ie>
2018-03-10 00:36:46 +0000

    Lab 6.2

--- Lab_6.2/roots_062.py
+++ Lab_6.2/roots_062.py
@@ -3,18 +3,24 @@
 import sys
 from math import sqrt
 
+
 def quadratic_roots(a, b, c):
-    return (-b + sqrt(b ** 2 - 4 * a * c)) / (2 * a), (-b - sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+    try:
+        return (-b + sqrt(b ** 2 - 4 * a * c)) / (2 * a), (-b - sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+    except ValueError:
+        pass
+
 
 def main():
     for line in sys.stdin:
         a, b, c = line.strip().split()
+        roots = quadratic_roots(int(a), int(b), int(c))
 
-        if quadratic_roots(int(a),int(b),int(c)):
-            print('r1 = {}, r2 = {}'.format())
+        if roots:
+            print('r1 = {}, r2 = {}'.format(roots[0], roots[1]))
+        else:
+            print(roots)
 
-
-    
 
 if __name__ == '__main__':
     main()
