506537e891db82604d8859a88e2d0b933137f083
Liam Carroll <liam.carroll36@mail.dcu.ie>
2018-03-10 14:38:29 +0000

    Lab 6.2

---
+++ Lab_6.2/guess_ex.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from random import randint
+
+top = 1000000
+bottom = 0
+z = randint(bottom, top)
+
+def guess(g):
+    if g == z:
+        return 0
+    elif g < z:
+        return -1
+    else:
+        return 1
+
+# Find z *efficiently* by calling guess() (and without peeking at z!)
+def find():
+    low = bottom
+    high = top
+
+    while low < high:
+        mid = (low + high) / 2
+
+        if guess(mid) == -1:
+            low = mid + 1
+        else:
+            high = mid
+
+    return low
+
+def main():
+    a = find()
+    if a == z:
+        print('Correct!')
+    else:
+        print('Incorrect!')
+    print('You said {:d} and answer is {:d}'.format(a, z))
+
+if __name__ == '__main__':
+    main()
