ca48d44cbf4052fd40588a8f3752d39941fe96b0
Liam Carroll <liamcarroll36@mail.dcu.ie>
2018-03-12 15:46:55 +0000

    Lab 6.2

--- Lab_6.2/guess_ex.py
+++ Lab_6.2/guess_ex.py
@@ -20,7 +20,7 @@ def find():
     high = top
 
     while low < high:
-        mid = (low + high) / 2
+        mid = (low + high) // 2
 
         if guess(mid) == -1:
             low = mid + 1
---
+++ Lab_6.2/patterns_ex.py
@@ -0,0 +1,27 @@
+from re import findall
+import sys
+
+from patterns_062 import date
+
+
+def main():
+
+    # Verify regular expressions are not overly long
+    assert(len(date) < 30)
+
+    s = sys.stdin.read()
+
+    datelist = findall(date, s)
+    print('Dates: {}'.format(datelist))
+
+    phonelist = findall(phone, s)
+    print('Phones: {}'.format(phonelist))
+
+    emaillist = findall(email, s)
+    print('Emails: {}'.format(emaillist))
+
+    ldatelist = findall(ldate, s)
+    print('Long dates: {}'.format(ldatelist))
+
+if __name__ == '__main__':
+    main()
