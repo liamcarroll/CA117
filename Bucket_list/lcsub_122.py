5f3ca830cdecd8ae2d6f4df952c737aa925ee2a1
Liam Carroll <liam.carroll36@mail.dcu.ie>
2018-03-14 13:52:39 +0000

    Bucketlist

--- Bucket_list/lcsub_122.py
+++ Bucket_list/lcsub_122.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import time
 
 
 def get_all_substrings(input_string):
@@ -9,16 +10,13 @@ def get_all_substrings(input_string):
 
 
 def main():
-    lines = []
-    for line in sys.stdin:
-        lines.append(line.strip())
-    s1, s2 = lines
-    substrings_s1 = set(get_all_substrings(s1))
-    substrings_s2 = set(get_all_substrings(s2))
-    common_substrings = substrings_s1.intersection(substrings_s2)
-    lcsub = max(common_substrings, key=len)
-    index_lcsub = s2.find(lcsub)
-    print('{} {} {}'.format(lcsub, len(lcsub), index_lcsub))
+    s1 = sys.stdin.readline().strip()
+    s2 = sys.stdin.readline().strip()
+    #Find all common substrings
+    common_substrings = set([s1[i:j + 1] for i in range(len(s1)) for j in range(i, len(s1)) if s1[i:j + 1] in s2])
+    #lcsub = max(common_substrings, key=len)
+    #index_lcsub = s2.find(lcsub)
+    #print('{} {} {}'.format(lcsub, len(lcsub), index_lcsub))
 
 
 if __name__ == '__main__':
