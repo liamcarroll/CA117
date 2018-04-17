5e0a434df2d9c1f3aaf04680e5203e1f00dc0935
Liam Carroll <liamcarroll36@mail.dcu.ie>
2018-04-17 12:45:17 +0100

    Lab 11.1

--- Lab_11.1/weight_111.py
+++ Lab_11.1/weight_111.py
@@ -6,30 +6,42 @@ class Weight(object):
         self.pounds = pounds
         self.ounces = ounces
 
-    def from_ounces(ounces):
-        pounds, ounces = divmod(ounces, 16)
-        return Weight(pounds, ounces)
+    @classmethod
+    def from_ounces(cls, ounces):
+        pounds, ounces = divmod(ounces, cls.OUNCES_PER_POUND)
+        return cls(pounds, ounces)
 
     def __str__(self):
-        return '{:d} pounds(s) and {:d} ounce(s)'.format(self.pounds, self.ounces)
+        return '{:d} pound(s) and {:d} ounce(s)'.format(self.pounds, self.ounces)
 
-    def pounds_to_ounces(self):
-        return (self.pounds * 16) + self.ounces
+    def to_ounces(self):
+        return (self.pounds * self.OUNCES_PER_POUND) + self.ounces
 
     def __ne__(self, other):
-        return self.pounds_to_ounces != other.pounds_to_ounces
+        return self.to_ounces() != other.to_ounces()
 
     def __eq__(self, other):
-        return self.pounds_to_ounces == other.pounds_to_ounces
+        return self.to_ounces() == other.to_ounces()
 
     def __lt__(self, other):
-        return self.pounds_to_ounces < other.pounds_to_ounces
+        return self.to_ounces() < other.to_ounces()
+
+    def __le__(self, other):
+        return self.to_ounces() <= other.to_ounces()
 
     def __gt__(self, other):
-        return self.pounds_to_ounces > other.pounds_to_ounces
+        return self.to_ounces() > other.to_ounces()
 
     def __ge__(self, other):
-        return self.pounds_to_ounces >= other.pounds_to_ounces
+        return self.to_ounces() >= other.to_ounces()
 
     def __add__(self, other):
-        (self.pounds_to_ounces + other.pounds_to_ounces)
+        return self.from_ounces(self.to_ounces() + other.to_ounces())
+
+    def __iadd__(self, other):
+        z = self + other
+        self.pounds, self.ounces = z.pounds, z.ounces
+        return self
+
+    def __mul__(self, n):
+        return self.from_ounces((self.to_ounces() * n))
